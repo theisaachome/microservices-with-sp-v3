eaf205774163ea813babf686f7dda25f1130472f
theisaachome <65864160+theisaachome@users.noreply.github.com>
2023-08-12 23:25:33 +0800

    Added Author services

--- author-service/src/main/java/com/isaachome/config/AppConfig.java
+++ author-service/src/main/java/com/isaachome/config/AppConfig.java
@@ -1,2 +1,14 @@
-package com.isaachome.config;public class AppConfig {
+package com.isaachome.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return  new ModelMapper();
+    }
 }
